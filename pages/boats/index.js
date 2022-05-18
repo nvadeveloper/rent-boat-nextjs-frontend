fb8da31eae0bcf34e20afd81288f0819b8b624ba
Nikita <nvadeveloper@gmail.com>
2022-05-18 23:59:06 +0300

    first commit

--- pages/boats/index.js
+++ pages/boats/index.js
@@ -1,4 +1,5 @@
 import PageHeading from "../../components/elements/PageHeading"
+import Link from "next/link"
 
 
 const Index = () => {
@@ -7,6 +8,9 @@ const Index = () => {
     <>
       <section className="container mx-auto px-4 sm:px-6 lg:px-8">
         <PageHeading>Катера</PageHeading>
+        <Link href='/boats/slug'>
+          <a className="bg-black hover:bg-gray-600 text-white py-3 px-6 rounded-full">To slug</a>
+        </Link>
       </section>
     </>
   )
