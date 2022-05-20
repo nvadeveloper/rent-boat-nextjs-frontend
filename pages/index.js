9c3062ef5f27444a9ae39252f97c0620e68e2927
Nikita <nvadeveloper@gmail.com>
2022-05-20 23:49:10 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Impression from "../components/indexPage/Impression"
 
 const Index = () => {
 
@@ -6,7 +7,7 @@ const Index = () => {
     <>
       <section className="container mx-auto px-4 sm:px-6 lg:px-8">
 
-        <p className="max-w-5xl mx-auto text-center text-4xl sm:text-6xl lg:text-8xl font-extrabold mt-10 mb-20 text-transparent bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-700">Откройте для себя совершенно новое путешествие!</p>
+        <p className="max-w-5xl mx-auto text-center text-4xl sm:text-6xl lg:text-8xl font-extrabold mt-10 mb-36 text-transparent bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-700">Откройте для себя совершенно новое путешествие!</p>
         
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
           <Link href='/boats'>
@@ -32,6 +33,7 @@ const Index = () => {
           </Link>
         </div>
 
+        <Impression />
         
       </section>
     </>
