c64ee134ec5e383d2db2fdb17da840c77966a0c3
Nikita <nvadeveloper@gmail.com>
2022-05-23 22:28:47 +0300

    first commit

--- components/elements/card/CardText.js
+++ components/elements/card/CardText.js
@@ -6,11 +6,11 @@ const CardText = ({ name, price, tag, person }) => {
         <>
             <div className="my-2">
                 <div className="flex justify-between items-center">
-                    <p className="font-bold text-xl">{name}</p>
+                    <p className="font-extrabold text-xl">{name}</p>
                     <p className="text-sm text-gray-900">{price} руб/час</p>
                 </div>
                 <div className="flex justify-between items-center">
-                    <p className="text-sm text-gray-600">{tag}</p>
+                    <p className="text-sm text-gray-400">{tag}</p>
                     <p className="text-sm text-gray-900">{person} человек</p>
                 </div>
             </div>
