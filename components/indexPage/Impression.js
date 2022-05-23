3e236f24111954ed497e7f94efe45deed4fe1bc4
Nikita <nvadeveloper@gmail.com>
2022-05-23 23:35:07 +0300

    first commit

--- components/indexPage/Impression.js
+++ components/indexPage/Impression.js
@@ -6,7 +6,7 @@ const Impression = () => {
         <>
             <PageHeading><p className="mt-10">Впечатления</p></PageHeading>
 
-            <div className="flex overflow-x-auto space-x-8 h-72 pb-4 snap-x">
+            <div className="flex overflow-x-auto no-scrollbar space-x-8 h-72 pb-4 snap-x">
 
                 <div className="rounded-2xl bg-gradient-to-r from-cyan-100 to-blue-100 snap-center hover:opacity-60">
                     <p className="w-72 sm:w-80 p-6 text-3xl font-extrabold font-montserrat">Аквадискотека</p>
