acfeab9960033bdd28a4c79adf02156fe480c553
Nikita <nvadeveloper@gmail.com>
2022-06-19 00:02:30 +0300

    🔧

--- components/home/Tagline.js
+++ components/home/Tagline.js
@@ -7,14 +7,14 @@ const Tagline = () => {
             <p className="max-w-3xl mx-auto text-center font-light sm:text-xl my-10 sm:mb-36 hidden sm:block text-gray-600">
                 Вы можете арендовать Катер, Теплоход, Яхту или даже Сапборд.
                 <br /> А так же вы можете найти в разделе Впечатление <br /> интерсное событиедля
-                себя и своих близких
+                себя и своих близких!
             </p>
 
             <p className="max-w-3xl mx-auto text-center text-sm font-medium my-10 sm:mb-36 block sm:hidden text-gray-600">
                 Вы можете арендовать Катер, Теплоход, Яхту <br />
                 или даже Сапборд. А так же вы можете найти <br />в разделе Впечатление интерсное
                 событие <br />
-                для себя и своих близких
+                для себя и своих близких!
             </p>
         </>
     );
