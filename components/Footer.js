1ce470b7b89ed4f608ebec48f27ba9f5bf1a54cb
Nikita <nvadeveloper@gmail.com>
2022-06-19 17:34:22 +0300

    🔧

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer>
             <section className="container mx-auto px-4 sm:px-6 lg:px-8 mt-12">
-                <div className="border-t flex justify-between py-4 sm:py-10 items-center">
+                <div className="border-t flex justify-between py-6 sm:py-10 items-center">
                     <Link href="/">
                         <a className="font-bold text-2xl text-blue-600 ">RentBoat</a>
                     </Link>
