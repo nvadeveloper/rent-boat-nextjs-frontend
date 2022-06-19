8639f31f818e13062896bab28d928e11213d314e
Nikita <nvadeveloper@gmail.com>
2022-06-19 17:37:34 +0300

    🔧

--- components/elements/card/CardSlider.js
+++ components/elements/card/CardSlider.js
@@ -26,7 +26,7 @@ const CardSlider = ({ images }) => {
                     nextEl: '.swiper-button-next',
                 }}
                 modules={[Pagination, Navigation]}
-                className="mySwiper rounded-xl bg-slate-50">
+                className="mySwiper rounded-xl bg-slate-100">
                 {images.map((image, key) => (
                     <SwiperSlide key={key}>
                         <div className="h-64 w-full relative">
